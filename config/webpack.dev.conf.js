30fc576fe6f17833c3ccbddf31c9da499fd55449
chengfeng xiong <xiongcf@broada.com>
2020-06-03 17:26:11 +0800

    init

---
+++ config/webpack.dev.conf.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack')
+
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map', // 调试源码
+  devServer: {
+    contentBase: path.join(__dirname, '../dist/'),
+    port: 8080,
+    hot: true,
+    overlay: true,
+    open: true,
+    proxy: {
+      '/comments': {
+        target: 'https://m.weibo.cn',
+        changeOrigin: true,
+        logLevel: 'debug',
+        headers: {
+          Cookie: ''
+        }
+      }
+    },
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+import './index.less'
+
+function render (APP) {
+  ReactDOM.render(<APP />, document.getElementById('app'))
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./app.js', () => render(App))
+}
